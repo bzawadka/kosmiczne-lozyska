e84d036562285123ed0d5213ac572373ef0eed02
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-04-06 00:58:38 +0800

    cleanup and better feedback

--- snippets/form.php
+++ snippets/form.php
@@ -5,45 +5,38 @@
     <label for="caseDiameter">caseDiameter:</label>
     <input type="text" name="caseDiameter" id="caseDiameter" value="35">
     <br>
-    <button type="submit">Submit</button>
+    <button type="submit" id="submit-button">Submit</button>
 </form>
 <div id="table-container"></div>
 
 <script>
 	document.addEventListener("DOMContentLoaded", function() {
-    var form = document.getElementById("myForm");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-// 		var data = {
-//             shaftDiameter: document.getElementById("shaftDiameter").value,
-//             caseDiameter: document.getElementById("caseDiameter").value
-//         };
-		$p1 = document.getElementById("shaftDiameter").value;
-		$p2 = document.getElementById("caseDiameter").value;
-        fetch("https://kosmiczne-lozyska.000webhostapp.com/api-page?shaftDiameter=" + $p1 + "&caseDiameter=" + $p2, {
-            method: "GET",
-// 			method: "POST",
-// 			headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(data)
-        })
-//         .then(response => response.json())
-		.then(response => response.text())
-        .then(data => {
-			const i1 = data.indexOf("<table>");
-			const i2 = data.indexOf("</table>");
+		var form = document.getElementById("myForm");
+		form.addEventListener("submit", function(event) {	
+			event.preventDefault();
+			document.getElementById("table-container").innerHTML = '<div/>';
+			document.getElementById("submit-button").disabled = true;
 			
-			const aTable = data.substring(i1, i2 + 8);
-			console.log("czesc");
-			console.log(aTable);
-			
-			var container = document.getElementById("table-container");
-            container.innerHTML = aTable;
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
-    });
-});
+			$p1 = document.getElementById("shaftDiameter").value;
+			$p2 = document.getElementById("caseDiameter").value;
+			fetch("https://kosmiczne-lozyska.000webhostapp.com/api-page?shaftDiameter=" + $p1 + "&caseDiameter=" + $p2, {
+				method: "GET",
+			})
+			.then(response => response.text())
+			.then(data => {
+				const i1 = data.indexOf("<table>");
+				const i2 = data.indexOf("</table>");
+				const dataTable = data.substring(i1, i2 + 8);
+
+				var container = document.getElementById("table-container");
+				container.innerHTML = dataTable;
+				
+				document.getElementById("submit-button").disabled = false;
+			})
+			.catch(error => {
+				console.error("Error:", error);
+				document.getElementById("submit-button").disabled = false;
+			});
+		});
+	});
 </script>
