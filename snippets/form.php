45bb195e34e268b8990adc88ad1820a919bd6fcc
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2023-04-06 01:30:29 +0800

    final touches for today

--- snippets/form.php
+++ snippets/form.php
@@ -1,9 +1,10 @@
 <form id="myForm">
-    <label for="shaftDiameter">shaftDiameter:</label>
+    <label for="shaft diameter">shaftDiameter:</label>
     <input type="text" name="shaftDiameter" id="shaftDiameter" value="25">
-    <br>
-    <label for="caseDiameter">caseDiameter:</label>
+    <label for="case diameter">caseDiameter:</label>
     <input type="text" name="caseDiameter" id="caseDiameter" value="35">
+	<label for="width">width:</label>
+    <input type="text" name="width" id="width" value="5">
     <br>
     <button type="submit" id="submit-button">Submit</button>
 	<label id="loading-label" hidden>loading...</label>
@@ -12,29 +13,31 @@
 
 <script>
 	document.addEventListener("DOMContentLoaded", function() {
-		var form = document.getElementById("myForm");
+		const form = document.getElementById("myForm");
 		form.addEventListener("submit", function(event) {	
 			event.preventDefault();
 			markAsLoading();
 			
-			$p1 = document.getElementById("shaftDiameter").value;
-			$p2 = document.getElementById("caseDiameter").value;
-			fetch("https://kosmiczne-lozyska.000webhostapp.com/api-page?shaftDiameter=" + $p1 + "&caseDiameter=" + $p2, {
+			const p1 = document.getElementById("shaftDiameter").value;
+			const p2 = document.getElementById("caseDiameter").value;
+			const p3 = document.getElementById("width").value;
+			const url = "https://kosmiczne-lozyska.000webhostapp.com/api-page?shaftDiameter=" + p1 + "&caseDiameter=" + p2 + "&width=" + p3;
+			fetch(url, {
 				method: "GET",
 			})
 			.then(response => response.text())
 			.then(data => {
 				const i1 = data.indexOf("<table>");
 				const i2 = data.indexOf("</table>");
-				const dataTable = data.substring(i1, i2 + 8);
+				const simmeringHtmlTable = data.substring(i1, i2 + 8);
 
-				var container = document.getElementById("table-container");
-				container.innerHTML = dataTable;
+				document.getElementById("table-container").innerHTML = simmeringHtmlTable;
 				markAsLoaded();
 			})
 			.catch(error => {
-				console.error("Error:", error);
 				markAsLoaded();
+				console.error("Error:", error);
+				document.getElementById("table-container").innerHTML = error.message
 			});
 		});
 	});
